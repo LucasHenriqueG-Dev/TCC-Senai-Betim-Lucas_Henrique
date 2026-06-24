967dac1ed13a25a074c2d827b27c70097c2e6471
LucasHenriqueG-Dev <lucass.guimaraes.dev@gmail.com>
2026-06-24 16:31:11 -0300

    tentativa de implementação do db

--- Frontend/scriptDavi.js
+++ Frontend/scriptDavi.js
@@ -65,3 +65,49 @@ btnDarkMode.addEventListener('click', (event) => {
         btnDarkMode.textContent = '☀️ Modo Claro';
     }
 });
+
+const SUPABASE_URL = 'https://qmpsvxtnqrzbgiskbrut.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_awciFXKFYcPV00RMDmKQFg_V4WWkZWM';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+const formDemandas = document.getElementById('formDemandas');
+const categoriaDemanda = document.getElementById('categoriaDemanda');
+
+const toggleIdentificar = document.getElementById('toggleIdentificar');
+const textoDemanda = document.getElementById('textoDemanda');
+const nomeUsuarioInput = document.getElementById('nomeUsuario');
+
+formDemandas.addEventListener('submit' , async (event) => {
+event.preventDefault();
+
+const campoArmadilha = document.getElementById('campoArmadilha').value;
+
+if (campoArmadilha !== "") {
+    console.log("Bot detectado.");
+    return;
+}
+
+let nomeFinal = 'Anônimo';
+if (toggleIdentificar.checked && nomeUsuarioInput.value.trim() !== "") {
+    nomeFinal = nomeUsuarioInput.value.trim();
+}
+
+const { data, error } = await supabase.from('demandas').insert([
+{
+categoria: categoriaDemanda.value,
+deseja_identificar: toggleIdentificar.checked,
+nome_usuario: nomeFinal,
+texto_demanda: textoDemanda.value,
+status: 'Pendente'
+}
+]);
+
+if (error) {
+    alert('Erro ao enviar o registro: ' + error.message);
+    console.error(error);
+} else {
+    alert('Registro enviado com sucesso para o Canal Fale Fácil!');
+    formDemandas.reset();
+}
+});
